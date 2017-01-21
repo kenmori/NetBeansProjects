6d5bf4318d427a546e09ff08b0e4b5b13f235c7f
森田賢二 <kenjimoritata@gmail.com>
2017-01-21 09:10:37 +0900

    singletonSessionBean2

---
+++  SingletonBean2/src/java/beans/BackingPage2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author moritakenji
+ */
+@Named
+@RequestScoped
+public class BackingPage2 {
+    @EJB
+    private SSBean ssb2;
+    public String next(){
+        return "page1";
+    }
+    public SSBean getSsb2(){
+        return ssb2;
+    }
+    public void setSsb1(SSBean ssb2){
+        this.ssb2 = ssb2;
+    }
+}
