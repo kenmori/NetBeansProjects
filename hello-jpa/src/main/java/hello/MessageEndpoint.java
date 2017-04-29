bb7370daac45968f8882be64a537f0e296b14452
森田賢二 <kenjimoritata@gmail.com>
2017-04-29 21:13:45 +0900

    Create servlet

---
+++ hello-jpa/src/main/java/hello/MessageEndpoint.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/**
+ *
+ * @author moritakenji
+ */
+@ApplicationScoped
+@Path("message")
+public class MessageEndpoint {
+    @Inject
+    MessageService messageService;
+    
+    @GET
+    @Path("add")
+    public Message add(@QueryParam("message") String message){
+        return messageService.create(message);
+    }
+    @GET
+    public List<Message> list(){
+        return messageService.findAll();
+    }
+}
---
+++ hello-jpa/src/main/java/hello/MessageService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+/**
+ *
+ * @author moritakenji
+ */
+@ApplicationScoped
+@Transactional
+public class MessageService {
+    @PersistenceContext
+    EntityManager entityManager;
+    
+    public Message create(String message){
+        Message entity = new Message();
+        entity.setMessage(message);
+        entityManager.persist(entity);
+        return entity;
+    }
+    public List<Message> findAll(){
+        return entityManager.createQuery("SELECT x FROM Message x", Message.class)
+                .getResultList();
+    }
+}
